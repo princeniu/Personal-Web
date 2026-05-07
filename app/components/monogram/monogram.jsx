95580cef85fdec39278302c5d00ff7582421e9ce
牛拙 <prince@niuzhuodeMacBook-Air.local>
2026-05-07 11:37:41 -0400

    feat: replace portfolio monogram with ZN logo

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -2,6 +2,9 @@ import { forwardRef, useId } from 'react';
 import { classes } from '~/utils/style';
 import styles from './monogram.module.css';
 
+const mainPath = 'M10 9h39v6H28L11 29v7h40v-6H29l23-21h-8L19 31V9h-9Z';
+const accentPath = 'M55 8h7v28h-7V19L35 36h-8l28-25V8Z';
+
 export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => {
   const id = useId();
   const clipId = `${id}monogram-clip`;
@@ -10,18 +13,20 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
     <svg
       aria-hidden
       className={classes(styles.monogram, className)}
-      width="48"
-      height="29"
-      viewBox="0 0 48 29"
+      width="52"
+      height="32"
+      viewBox="0 0 72 44"
       ref={ref}
       {...props}
     >
       <defs>
         <clipPath id={clipId}>
-          <path d="M0 0h6.5a6 6 0 0 1 5.2 3.1L19.4 17l4-9L19 0h6.5a6 6 0 0 1 5.2 3.1L39.5 19 35 29 24.5 10 16 29 0 0Zm46.7 2.8A2 2 0 0 0 45 0h-7l5.5 10 3.2-7.2Z" />
+          <path d={mainPath} />
+          <path d={accentPath} />
         </clipPath>
       </defs>
-      <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
+      <path className={styles.base} d={mainPath} />
+      <path className={styles.accent} d={accentPath} />
       {highlight && (
         <g clipPath={`url(#${clipId})`}>
           <rect className={styles.highlight} width="100%" height="100%" />
